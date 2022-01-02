c34c4b8e38c52c7e2dec25587f6e0ca3d0434ea6
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-01-02 18:17:43 +0000

    Added base authpage

---
+++ src/pages/auth/AuthPage.js
@@ -0,0 +1,63 @@
+import React from "react";
+import BasePublicPage from "../BasePublicPage";
+import {
+  Container,
+  Flex,
+  GridItem,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+const AuthPage = ({ authForm, headerText }) => {
+  return (
+    <BasePublicPage>
+      <Container
+        as={SimpleGrid}
+        maxW={"9xl"}
+        columns={{ base: 1, sm: 1, md: 1, lg: 1, xl: 2 }}
+        spacing={{ base: 4, xl: 32 }}
+        py={10}
+        mt={"5%"}
+        justifyItems={"center"}
+      >
+        <GridItem>
+          <Stack color={"white"} direction={"row"} align={"center"} spacing={0}>
+            <Container m={"5%"} mr={0} w={"auto"}>
+              <Text
+                fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "5xl" }}
+                fontWeight={"bold"}
+              >
+                {headerText}
+              </Text>
+              <Text fontSize={{ base: "2xl", sm: "2xl", md: "2xl", lg: "3xl" }}>
+                The Lonely Project
+              </Text>
+              <Text mt={"5%"}>
+                Let’s get you connected with similarly minded people
+              </Text>
+            </Container>
+            <Flex>
+              <Image
+                alt={"Login Image"}
+                objectFit={"cover"}
+                src={require("../../assets/media/headphone.png")}
+              />
+            </Flex>
+          </Stack>
+        </GridItem>
+        <GridItem rowSpan={2} w={"xl"}>
+          {authForm}
+        </GridItem>
+        <GridItem>
+          <img
+            src={require("../../assets/media/people.png")}
+            alt={"Person sitting at computer"}
+          />
+        </GridItem>
+      </Container>
+    </BasePublicPage>
+  );
+};
+export default AuthPage;
