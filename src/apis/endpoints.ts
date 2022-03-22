5037102188cb65b0e4053e955d29330587d8aad7
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-03-22 20:32:52 +0000

    Update endpoints.ts

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -14,7 +14,7 @@ export const USER_PROFILE_PROMPTS_ENDPOINT = "/user/profile/prompts";
 export const USER_PROFILE_ADD_PROMPT_ENDPOINT = "/user/profile/prompt";
 export const USER_PROFILE_DELETE_PROMPT_ENDPOINT =
   USER_PROFILE_ADD_PROMPT_ENDPOINT;
-export const GET_POTENTIAL_MATCHES = "/user/profiles";
+export const GET_POTENTIAL_MATCHES = "/user/profile/matches ";
 export const GET_PENDING_MATCHES = "/user/profile/connections/pending";
 export const USER_PROFILE_SEND_CONNECTION_REQUEST = "/user/profile/%s/connect";
 export const CHAT_GET_CONVERSATIONS = "/chat/chatrooms";
